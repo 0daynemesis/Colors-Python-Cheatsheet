c0f341ce35106c1714fd44401ccddbaa20b1650f
0daynemesis <137519956+0daynemesis@users.noreply.github.com>
2024-09-03 22:09:16 +0100

    Update colors.py

--- colors.py
+++ colors.py
@@ -1,3 +1,154 @@
 print('Colors Python Code Cheatsheet')
 
+# Reset
+Color_Off="\033[0m"       # Text Reset
 
+# Regular Colors
+Black="\033[0;30m"        # Black
+Red="\033[0;31m"          # Red
+Green="\033[0;32m"        # Green
+Yellow="\033[0;33m"       # Yellow
+Blue="\033[0;34m"         # Blue
+Purple="\033[0;35m"       # Purple
+Cyan="\033[0;36m"         # Cyan
+White="\033[0;37m"        # White
+
+# Bold
+BBlack="\033[1;30m"       # Black
+BRed="\033[1;31m"         # Red
+BGreen="\033[1;32m"       # Green
+BYellow="\033[1;33m"      # Yellow
+BBlue="\033[1;34m"        # Blue
+BPurple="\033[1;35m"      # Purple
+BCyan="\033[1;36m"        # Cyan
+BWhite="\033[1;37m"       # White
+
+# Underline
+UBlack="\033[4;30m"       # Black
+URed="\033[4;31m"         # Red
+UGreen="\033[4;32m"       # Green
+UYellow="\033[4;33m"      # Yellow
+UBlue="\033[4;34m"        # Blue
+UPurple="\033[4;35m"      # Purple
+UCyan="\033[4;36m"        # Cyan
+UWhite="\033[4;37m"       # White
+
+# Background
+On_Black="\033[40m"       # Black
+On_Red="\033[41m"         # Red
+On_Green="\033[42m"       # Green
+On_Yellow="\033[43m"      # Yellow
+On_Blue="\033[44m"        # Blue
+On_Purple="\033[45m"      # Purple
+On_Cyan="\033[46m"        # Cyan
+On_White="\033[47m"       # White
+
+# High Intensty
+IBlack="\033[0;90m"       # Black
+IRed="\033[0;91m"         # Red
+IGreen="\033[0;92m"       # Green
+IYellow="\033[0;93m"      # Yellow
+IBlue="\033[0;94m"        # Blue
+IPurple="\033[0;95m"      # Purple
+ICyan="\033[0;96m"        # Cyan
+IWhite="\033[0;97m"       # White
+
+# Bold High Intensty
+BIBlack="\033[1;90m"      # Black
+BIRed="\033[1;91m"        # Red
+BIGreen="\033[1;92m"      # Green
+BIYellow="\033[1;93m"     # Yellow
+BIBlue="\033[1;94m"       # Blue
+BIPurple="\033[1;95m"     # Purple
+BICyan="\033[1;96m"       # Cyan
+BIWhite="\033[1;97m"      # White
+
+# High Intensty backgrounds
+On_IBlack="\033[0;100m"   # Black
+On_IRed="\033[0;101m"     # Red
+On_IGreen="\033[0;102m"   # Green
+On_IYellow="\033[0;103m"  # Yellow
+On_IBlue="\033[0;104m"    # Blue
+On_IPurple="\033[10;95m"  # Purple
+On_ICyan="\033[0;106m"    # Cyan
+On_IWhite="\033[0;107m"   # White
+
+
+
+
+
+
+
+
+# Reset
+print('Color_Off = Reset Color')
+
+# Regular Colors
+print(Black + 'Black')
+print('Red')
+print('Green')
+print('Yellow')
+print('Blue')
+print('Purple')
+print('Cyan')
+print('White')
+
+print('# Bold')
+print('BBlack')
+print('BRed')
+print('BGreen')
+print('BYellow')
+print('BBlue')
+print('BPurple')
+print('BCyan')
+print('BWhite')
+
+print('# Underline')
+print('UBlack')
+print('URed')
+print('UGreen')
+print('UYellow')
+print('UBlue')
+print('UPurple')
+print('UCyan')
+print('UWhite')
+
+print('# Background')
+print('On_Black')
+print('On_Red')
+print('On_Green')
+print('On_Yellow')
+print('On_Blue')
+print('On_Purple')
+print('On_Cyan')
+print('On_White')
+
+print('# High Intensty')
+print('IBlack')
+print('IRed')
+print('IGreen')
+print('IYellow')
+print('IBlue')
+print('IPurple')
+print('ICyan')
+print('IWhite')
+
+print('# Bold High Intensty')
+print('BIBlack')
+print('BIRed')
+print('BIGreen')
+print('BIYellow')
+print('BIBlue')
+print('BIPurple')
+print('BICyan')
+print('BIWhite')
+
+print('# High Intensty backgrounds')
+print('On_IBlack')
+print('On_IRed')
+print('On_IGreen')
+print('On_IYellow')
+print('On_IBlue')
+print('On_IPurple')
+print('On_ICyan')
+print('On_IWhite')
